895384ff197042587fc78b38b942b8e2f3e2f5b1
rahmansunny071 <rahmansunny071@gmail.com>
2019-01-27 13:40:08 -0600

    docs folder added

---
+++ docs/precache-manifest.8fcefcdae98df59f41850e0598a202e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carousel-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f2e2c8bd5209ceee6f22",
+    "url": "/carousel-app/static/js/runtime~main.f2e2c8bd.js"
+  },
+  {
+    "revision": "36b643939c8517e5e228",
+    "url": "/carousel-app/static/js/main.36b64393.chunk.js"
+  },
+  {
+    "revision": "8abe52226c12c78ed704",
+    "url": "/carousel-app/static/js/1.8abe5222.chunk.js"
+  },
+  {
+    "revision": "36b643939c8517e5e228",
+    "url": "/carousel-app/static/css/main.6d69678e.chunk.css"
+  },
+  {
+    "revision": "3a92db2b1cfcdfd81e50e1275dc72e3e",
+    "url": "/carousel-app/index.html"
+  }
+];
